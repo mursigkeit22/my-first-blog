cd96e73b471c2771262d2db480f1538a06b4e971
mursigkeit22 <lera_shel@mail.ru>
2019-06-17 06:52:52 +0300

    comment model

--- mysite/urls.py
+++ mysite/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('', include('blog.urls')),
     path('accounts/login/', views.LoginView.as_view(), name='login'),
     path('accounts/logout/', views.LogoutView.as_view(next_page='/'), name='logout'),
+
 ]
